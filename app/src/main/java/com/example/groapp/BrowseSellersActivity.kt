ba391048272ac66750441105de53e22e5880ce25
Shabina <shabinafarveen@gmail.com>
2023-03-18 13:03:00 +0530

    shabs - home except gardens done

---
+++ app/src/main/java/com/example/groapp/BrowseSellersActivity.kt
@@ -0,0 +1,11 @@
+package com.example.groapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BrowseSellersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_browse_sellers)
+    }
+}
