de2e26221d6d5d402d951f69d0d74464049ed09d
Shabina <shabinafarveen@gmail.com>
2023-03-19 12:09:57 +0530

    shabs - Add/Edit/Delete Items Completed

---
+++ app/src/main/java/com/example/groapp/RateItemActivity.kt
@@ -0,0 +1,11 @@
+package com.example.groapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RateItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rate_item)
+    }
+}
