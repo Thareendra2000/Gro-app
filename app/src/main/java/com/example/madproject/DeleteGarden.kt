9e691a7a9e77b417238b11e7b301982a6809f3bd
Thareendra2000 <thareendra2000@gmail.com>
2023-04-30 06:49:20 +0530

    Fixed the header theame

---
+++ app/src/main/java/com/example/madproject/DeleteGarden.kt
@@ -0,0 +1,11 @@
+package com.example.madproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeleteGarden : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delete_garden)
+    }
+}
