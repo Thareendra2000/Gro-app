e55046f5540764f515e2e8dda15440a65a03d142
Thareendra2000 <thareendra2000@gmail.com>
2023-05-05 09:33:30 +0530

    Added AddGardenActivity

---
+++ app/src/main/java/com/example/groapp/Model/GardenModel.kt
@@ -0,0 +1,12 @@
+package com.example.groapp.Model
+
+data class GardenModel(
+    var gardenId: String? = null,
+    var name: String? = null,
+    var address: String? = null,
+    var phoneNo: String? = null,
+    var location: String? = null,
+    var area: String? = null,
+    var description: String? = null
+)
+
