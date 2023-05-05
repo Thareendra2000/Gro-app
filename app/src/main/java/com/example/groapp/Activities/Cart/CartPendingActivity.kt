fa9dbabbdcff0af2836bbc6ebba4ee8cc38f8114
saputhebeast <sapumalwijekoon4@gmail.com>
2023-05-05 21:12:48 +0530

    Cart navigation completed

--- app/src/main/java/com/example/groapp/Activities/Cart/CartPendingActivity.kt
+++ app/src/main/java/com/example/groapp/Activities/Cart/CartPendingActivity.kt
@@ -1,12 +1,39 @@
 package com.example.groapp.Activities.Cart
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import com.example.groapp.Activities.HomeActivity
+import com.example.groapp.Activities.MainActivity
 import com.example.groapp.R
 
 class CartPendingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart_pending)
+
+        // cart navigation
+        var backImg : ImageView = findViewById(R.id.backImg)
+        backImg.setOnClickListener{
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+        var tvPending : Button = findViewById(R.id.tvPending)
+        tvPending.setOnClickListener{
+            val intent = Intent(this, CartPendingActivity::class.java)
+            startActivity(intent)
+        }
+        var tvPickUp : Button = findViewById(R.id.tvPickUp)
+        tvPickUp.setOnClickListener{
+            val intent = Intent(this, CartPickUpsActivity::class.java)
+            startActivity(intent)
+        }
+        var tvCompleted : Button = findViewById(R.id.tvCompleted)
+        tvCompleted.setOnClickListener{
+            val intent = Intent(this, CartCompletedActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
