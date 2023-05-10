4c749956ed1925f5046926eb65405ce085eb13c5
MiHi <mrmihiraj@gmail.com>
2023-05-11 00:54:04 +0530

    Added authentication

--- app/src/main/java/com/example/groapp/Activities/MainActivity.kt
+++ app/src/main/java/com/example/groapp/Activities/MainActivity.kt
@@ -1,12 +1,63 @@
 package com.example.groapp.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.groapp.Activities.Volunteer.VolunteerMain
 import com.example.groapp.R
+import com.example.groapp.Services.UserSingleton
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var volunteerfun: Button
+    private lateinit var signout: Button
+    private lateinit var profile: Button
+    private lateinit var garden: Button
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form_template)
+        setContentView(R.layout.activity_main)
+
+        val user = Firebase.auth.currentUser
+        user?.let {
+            UserSingleton.name = it.displayName
+            UserSingleton.email = it.email
+            UserSingleton.uid = it.uid
+        }
+
+
+        volunteerfun = findViewById(R.id.volunteer)
+        signout = findViewById(R.id.signout)
+        profile = findViewById(R.id.profile)
+        garden = findViewById(R.id.garden)
+
+
+
+
+        volunteerfun.setOnClickListener {
+            val intent = Intent(this, VolunteerMain::class.java)
+            startActivity(intent)
+        }
+
+        signout.setOnClickListener {
+            Firebase.auth.signOut()
+        }
+
+        profile.setOnClickListener {
+            val intent = Intent(this, ProfileMain::class.java)
+            startActivity(intent)
+        }
+
+//        garden.setOnClickListener {
+//            val intent = Intent(this, GardenActivity::class.java)
+//            startActivity(intent)
+//        }
+
     }
 }
