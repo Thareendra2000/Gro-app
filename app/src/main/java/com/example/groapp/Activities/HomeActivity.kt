c285de2243c8f42a76334e2bcf91cb00b4f1958e
MiHi <mrmihiraj@gmail.com>
2023-05-11 08:42:20 +0530

    Added the previous home

--- app/src/main/java/com/example/groapp/Activities/HomeActivity.kt
+++ app/src/main/java/com/example/groapp/Activities/HomeActivity.kt
@@ -3,16 +3,28 @@ package com.example.groapp.Activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import com.example.groapp.Activities.Cart.CartPendingActivity
 import com.example.groapp.Activities.MarketPlace.MarketPlaceActivity
+import com.example.groapp.Activities.Volunteer.VolunteerMain
 import com.example.groapp.R
+import com.example.groapp.Services.UserSingleton
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val user = Firebase.auth.currentUser
+        user?.let {
+            UserSingleton.name = it.displayName
+            UserSingleton.email = it.email
+            UserSingleton.uid = it.uid
+        }
 
         var tvMarketPlace : LinearLayout = findViewById(R.id.tvMarketPlace)
         tvMarketPlace.setOnClickListener{
@@ -36,5 +48,26 @@ class HomeActivity : AppCompatActivity() {
             val intent = Intent(this, MyProfileActivity::class.java)
             startActivity(intent)
         }
+
+
+        var volunteerfun : Button = findViewById(R.id.volunteer)
+        volunteerfun.setOnClickListener {
+            val intent = Intent(this, VolunteerMain::class.java)
+            startActivity(intent)
+        }
+        var signout : Button = findViewById(R.id.signout)
+        signout.setOnClickListener {
+            Firebase.auth.signOut()
+        }
+        var profile : Button = findViewById(R.id.profile)
+        profile.setOnClickListener {
+            val intent = Intent(this, ProfileMain::class.java)
+            startActivity(intent)
+        }
+        //var volunteerfun : Button = findViewById(R.id.volunteer)
+//        garden.setOnClickListener {
+//            val intent = Intent(this, GardenActivity::class.java)
+//            startActivity(intent)
+//        }
     }
 }
